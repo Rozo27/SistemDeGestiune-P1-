701dc2d155dade902bade51e2416e7aa05f5e87d
Rozo27 <andreearozoleanu27@gmail.com>
2020-06-13 00:52:35 +0300

    Update FindSubString.java

--- FindSubString.java
+++ FindSubString.java
@@ -2,7 +2,12 @@ import java.util.ArrayList;
 public class FindSubString {
 
     public static ArrayList<Guest> containsStringInName(ArrayList<Guest> listaCompleta,String string){
+        try{
         string=string.toLowerCase();
+        } catch(NullPointerException e){
+            System.out.println("Sirul de caractere dat ca parametrii este invalid");
+            return null;
+        }
         int i;
         ArrayList<Guest> rezultat=new ArrayList<>();
         for(i=0;i<listaCompleta.size();i++){
